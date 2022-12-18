2050311b8bbde0f4f6bed221f1494b4a50577a75
wramadan7-org <wramadan1203@gmail.com>
2022-12-18 21:45:59 +0700

    docker prune

---
+++ docker-prune.sh
@@ -0,0 +1,5 @@
+## Docker prune is to delete the network or container or volume or image when it not used
+docker nameofcommand(network, volume, image, container) prune
+
+## If we want to delete all but not the volume use this command
+docker system prune
